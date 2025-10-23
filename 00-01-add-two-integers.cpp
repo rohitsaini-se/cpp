52da99e9617fd7343639dd368937c3fc05312989
Rohit Saini <emailrohitsaini@gmail.com>
2025-10-23 12:39:51 +0530

    program to add two integers

---
+++ 00-01-add-two-integers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int num1, num2;
+  cout << "Enter two integers: \n";
+  cin >> num1 >> num2;
+
+  int sum = num1 + num2;
+  cout << "Sum of the entered integers is: " << sum;
+
+  return 0;
+}
